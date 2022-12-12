8733cf3748b7f49360a309041b2a8bdd56d6137d
Luis <79956765+11LuisQuispe11@users.noreply.github.com>
2022-12-11 22:48:34 -0500

    Trabajo de verificaicion

---
+++ back con front/config.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+  'db' => [
+    'host' => 'localhost',
+    'user' => 'root',
+    'pass' => 'root',
+    'name' => 'healthy2',
+    'options' => [
+      PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+    ]
+  ]
+];
